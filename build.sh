a8c0af83645f750780588a8525217cc442f59021
mehmetcan <mehmetcan.karabulut96@gmail.com>
2026-04-18 17:13:42 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-# Hata durumunda dur
+# Herhangi bir hata oluşursa betiği durdurur
 set -o errexit
 
-# Sistem kütüphanelerini güncelle ve eksik olanları yükle
-# Bu satır libGLESv2 ve libGL hatalarını çözer
-apt-get update && apt-get install -y libgl1-mesa-glx libgles2 libglib2.0-0
+# 1. Sistem paketlerini güncelle ve MediaPipe/OpenCV için gereken bağımlılıkları yükle
+# Bu kısım libGLESv2, libGL ve libglib hatalarını çözer
+apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    libgles2 \
+    libglib2.0-0
 
-# Bağımlılıkları kur
-pip install --upgrade pip setuptools wheel
+# 2. Pip ve temel kurulum araçlarını güncelle
+python -m pip install --upgrade pip setuptools wheel
+
+# 3. Bağımlılıkları kur
 pip install -r requirements.txt
